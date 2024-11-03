d9a33da8c584d391f9ff49e46bfd16d1c49c2109
gereas <gereas@vk.com>
2024-11-03 14:48:28 +0300

    add changes

--- parser_1.py
+++ parser_1.py
@@ -4,55 +4,23 @@ import aiohttp
 import sqlite3
 
 
-msg_BTC = \
-{
-  "jsonrpc" : "2.0",
-  "id" : 8106,
-  "method" : "public/ticker",
-  "params" : {
-    "instrument_name" : "BTC-PERPETUAL"
-  }
-}
-
-msg_ETH = \
-{
-  "jsonrpc" : "2.0",
-  "id" : 8106,
-  "method" : "public/ticker",
-  "params" : {
-    "instrument_name" : "ETH-PERPETUAL"
-  }
-}
-conn = sqlite3.connect('data_pars.db')
-cursor = conn.cursor()
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS my_table (
-    timestamp TEXT,
-    index_price REAL NOT NULL,
-    instrument_name TEXT NOT NULL
-)
-''')
-conn.commit()
-conn.close()
-
-
-data_result = dict()
-sorted_data = dict()
-
-
+#функция для парсинга данных и внесения данных в бд
 async def call_api(session, msg):
     async with session.ws_connect('wss://test.deribit.com/ws/api/v2') as ws:
         await ws.send_json(msg)
+
+        data_result = dict()
+        sorted_data = dict()
 
         response = await ws.receive()
         data = json.loads(response.data)
         data_result = data['result']
         
         sorted_data = { 
-            "timestamp":data_result['timestamp'],
-            "index_price":data_result['index_price'],
-            "instrument_name":data_result['instrument_name'],
-            }
+          "timestamp":data_result['timestamp'],
+          "index_price":data_result['index_price'],
+          "instrument_name":data_result['instrument_name'],
+          }
         
         conn = sqlite3.connect('data_pars.db')
         cursor = conn.cursor()
@@ -63,15 +31,47 @@ async def call_api(session, msg):
         conn.commit()
         conn.close()
         
-        print(sorted_data)
-            
 
 async def main():
-        async with aiohttp.ClientSession() as session:
-            while True:
-                await call_api(session, msg_BTC)
-                await call_api(session, msg_ETH)
-                await asyncio.sleep(10)
+    #переменные для отправки json запроса на сервис
+    msg_BTC = \
+    {
+      "jsonrpc" : "2.0",
+      "id" : 8106,
+      "method" : "public/ticker",
+      "params" : {
+        "instrument_name" : "BTC-PERPETUAL"
+      }
+    }
+    msg_ETH = \
+    {
+      "jsonrpc" : "2.0",
+      "id" : 8106,
+      "method" : "public/ticker",
+      "params" : {
+        "instrument_name" : "ETH-PERPETUAL"
+      }
+    }
+
+    #создание бд если она не была создана ранее
+    conn = sqlite3.connect('data_pars.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS my_table (
+        timestamp TEXT,
+        index_price REAL NOT NULL,
+        instrument_name TEXT NOT NULL
+    )
+    ''')
+    conn.commit()
+    conn.close()
+
+    #вызов функций для запроса данных
+    async with aiohttp.ClientSession() as session:
+      while True:
+        await call_api(session, msg_BTC)
+        await call_api(session, msg_ETH)
+        await asyncio.sleep(60)
 
 
 asyncio.get_event_loop().run_until_complete(main())
--- parser_1_v2.py
+++ parser_1_v2.py
@@ -4,28 +4,9 @@ import aiohttp
 import sqlite3
 
 
-name_val = ['BTC','ETH']
-conn = sqlite3.connect('data_pars.db')
-cursor = conn.cursor()
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS my_table (
-    timestamp TEXT,
-    index_price REAL NOT NULL,
-    instrument_name TEXT NOT NULL
-)
-''')
-conn.commit()
-conn.close()
-
-
-data_result = dict()
-sorted_data = dict()
-
-name_val = ['BTC','ETH']
-ticker_name = list()
-
-
 async def take_all_ticker_name(session):
+    name_val = ['BTC','ETH']
+    ticker_name = list()
     async with session.ws_connect('wss://test.deribit.com/ws/api/v2') as ws:
         for i in name_val:
             msg = \
@@ -45,8 +26,14 @@ async def take_all_ticker_name(session):
             for i in range(len(data['result'])):
                 ticker_name.append(data['result'][i]['instrument_name'])
 
-     
-async def call_api(session):
+    return(ticker_name)
+
+
+#функция для парсинга данных и внесения данных в бд
+async def call_api(session, ticker_name):
+    data_result = dict()
+    sorted_data = dict()
+
     async with session.ws_connect('wss://test.deribit.com/ws/api/v2') as ws:
         for i in ticker_name:
             msg = \
@@ -60,11 +47,10 @@ async def call_api(session):
                 }
             
             await ws.send_json(msg)
-
             response = await ws.receive()
             data = json.loads(response.data)
+
             data_result = data['result']
-            
             sorted_data = { 
                 "timestamp":data_result['timestamp'],
                 "index_price":data_result['index_price'],
@@ -80,17 +66,26 @@ async def call_api(session):
             conn.commit()
             conn.close()
             
-            print(sorted_data)
-            
 
 async def main():
-        async with aiohttp.ClientSession() as session:
-            await take_all_ticker_name(session)
-            while True:
-                await call_api(session)
-                await asyncio.sleep(20)
+    #создание бд если она не была создана ранее
+    conn = sqlite3.connect('data_pars.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS my_table (
+        timestamp TEXT,
+        index_price REAL NOT NULL,
+        instrument_name TEXT NOT NULL)
+    ''')
+    conn.commit()
+    conn.close()
 
-
+    #вызов функций для запроса данных
+    async with aiohttp.ClientSession() as session:
+        data = await take_all_ticker_name(session)
+        while True:
+            await call_api(session, data)
+            await asyncio.sleep(20)
 
 
 asyncio.get_event_loop().run_until_complete(main())
